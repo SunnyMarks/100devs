19077023c273302b2cb6d7091f6b836f2f31b656
MONSTARV2\marki <markimarksstudio@gmail.com>
2021-03-24 19:24:59 -0500

    server set up

--- crud_practice/api-project/server.js
+++ crud_practice/api-project/server.js
@@ -1,6 +1,22 @@
+const { request, response } = require('express');
 const express = require('express');
 const app = express();
+const PORT = 8000;
+
+let savage = {
+    'age': 28,
+    'birthName': 'Sheyaa Bin Abraham-Joseph',
+    'birthLocation': 'London, England'
+}
 
 app.get('/', (request, response) => {
-    response.sendFile(/Library/art/2021/projects/projects_current/PracticeEveryCycle/leonbootcamp/hundreddevs/crud_practice/api-project + '/index.html')
+    response.sendFile(__dirname + '/index.html')
+});
+
+app.get('/api/savage', (request, response) => {
+    response.json(savage);
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on ${PORT}`)
 })
