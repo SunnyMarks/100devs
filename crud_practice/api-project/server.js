b01d10604dfb721127bdf592bfc0015cb8ab3690
MONSTARV2\marki <markimarksstudio@gmail.com>
2021-03-30 17:06:21 -0500

    crud example using mongodb

--- crud_practice/api-project/server.js
+++ crud_practice/api-project/server.js
@@ -1,22 +1,48 @@
 const { request, response } = require('express');
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
-const PORT = 8000;
+const PORT = 3000;
 
-let savage = {
-    'age': 28,
-    'birthName': 'Sheyaa Bin Abraham-Joseph',
-    'birthLocation': 'London, England'
-}
+MongoClient.connect('mongodb+srv://yoda:yodaisop@cluster0.pdtpo.mongodb.net/OrgTest001?retryWrites=true&w=majority', { 
+    useUnifiedTopology: true 
+}) 
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('OrgTest001')
+        const quotesCollection = db.collection('quotes')
+        //Handlers-------
+        app.listen(PORT, ()=>{
+            console.log(`Server running on ${PORT}`)
+        })
+        ///Middlewares ------
+        app.use(bodyParser.urlencoded({ extended: true }))
+            console.log('May Node be with you');
+        //Routes -----
+        app.get('/', (request, response) => {
+            db.collection('quotes').find().toArray()
+            
+            .then(results => {
+                console.log(results)
+            })
+            .catch(error => console.error(error))
+           
+            response.sendFile(__dirname + '/index.html')
+        });
+        app.post('/quotes', (request, response) => {
+            quotesCollection.insertOne(request.body)
+                .then(result => {
+                    response.redirect('/')
+                })
+                .catch(error => console.error(error))
+            //console.log(request.body)
+        })
+        
+    })
 
-app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/index.html')
-});
+    .catch(error => console.error(error))
+    
 
-app.get('/api/savage', (request, response) => {
-    response.json(savage);
-})
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on ${PORT}`)
-})
+//Place body-parser before your CRUD handlers!
